7e56d154b938452e5a584d17035d9bee249fd6cc
AmitaiRosenbaum <amitai.rosenbaum@outlook.com>
2025-06-08 15:25:21 +1000

    connect selector to documents

--- src/sections/SongInput/SongSubmit.tsx
+++ src/sections/SongInput/SongSubmit.tsx
@@ -1,6 +1,16 @@
 import { Button } from "@mui/material";
 import type { SongType } from "../../types/songs";
+import type React from "react";
 
 export default function SongSubmit({ song }: { song: SongType | null }) {
-  return <Button variant="contained">Find Song</Button>;
+  const openSong = (_event: React.MouseEvent) => {
+    if (!song) return null;
+
+    window.open(song.path);
+  };
+  return (
+    <Button variant="contained" onClick={(event) => openSong(event)}>
+      Find Song
+    </Button>
+  );
 }
