4f4871184ab18a852c8c2e78f619d0ba8e51f729
AmitaiRosenbaum <amitai.rosenbaum@outlook.com>
2025-06-10 22:40:55 +1000

    refactor with generics

--- back-end/songAPI/songAPI/songs/views.py
+++ back-end/songAPI/songAPI/songs/views.py
@@ -1,27 +1,13 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.decorators import api_view
-from rest_framework.parsers import JSONParser
-from rest_framework.response import Response
-from rest_framework import status
 from songAPI.songs.models import Song
 from songAPI.songs.serializers import SongSerializer
+from rest_framework import generics
 
-# Create your views here.
-@api_view(['GET', 'POST'])
-def song_list(request):
-  """
-  List all songs
-  """
-  if request.method == 'GET':
-    songs = Song.objects.all()
-    serializer = SongSerializer(songs, many = True)
-    return Response(serializer.data)
 
-  elif request.method == 'POST':
-    serializer = SongSerializer(data=request.data)
-    if serializer.is_valid():
-      serializer.save()
-      return Response(serializer.data, status=status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class SongList(generics.ListCreateAPIView):
+  queryset = Song.objects.all()
+  serializer_class = SongSerializer
+
+
+class SongDetail(generics.RetrieveUpdateDestroyAPIView):
+  queryset = Song.objects.all()
+  serializer_class = SongSerializer
