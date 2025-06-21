8617c15d18298648732c9686b9dcb85db28dbb4c
AmitaiRosenbaum <amitai.rosenbaum@outlook.com>
2025-06-21 17:49:22 +1000

    set env variables

--- front-end/src/utils/axios.ts
+++ front-end/src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios, { type AxiosRequestConfig } from "axios";
 
 const axiosService = axios.create({
-  baseURL: "http://localhost:8000/",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default axiosService;
