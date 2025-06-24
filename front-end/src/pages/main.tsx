4e11286a3e625c316fc749cf8b93271578ba568d
AmitaiRosenbaum <amitai.rosenbaum@outlook.com>
2025-06-24 23:12:17 +1000

    add all songs link

--- front-end/src/pages/main.tsx
+++ front-end/src/pages/main.tsx
@@ -3,20 +3,26 @@ import SongInputComponent from "../sections/SongInput";
 import SongSuggestionSlider from "../sections/SongSuggestion";
 import { useAllSongs } from "../api/hooks/songs";
 import SongContext from "../contexts/SongContext";
+import Footer from "../sections/Footer";
 
 export default function MainPage() {
   const { data: songs, isLoading } = useAllSongs();
 
   return (
-    <Box sx={{ pl: 30, pr: 30 }}>
-      <Stack spacing={2} direction="column">
-        <SongContext value={{ data: songs, isLoading }}>
-          <Typography variant="h2">Brasileiro</Typography>
-          <SongInputComponent />
-          <Typography variant="h4">Suggestions for you</Typography>
-          <SongSuggestionSlider />
-        </SongContext>
-      </Stack>
-    </Box>
+    <>
+      <Box sx={{ pl: 30, pr: 30 }}>
+        <Stack spacing={2} direction="column">
+          <SongContext value={{ data: songs, isLoading }}>
+            <Typography variant="h2">Brasileiro</Typography>
+            <SongInputComponent />
+            <Typography variant="h4">Suggestions for you</Typography>
+            <SongSuggestionSlider />
+            <Box sx={{ pt: 10 }}>
+              <Footer />
+            </Box>
+          </SongContext>
+        </Stack>
+      </Box>
+    </>
   );
 }
---
+++ front-end/src/sections/Footer.tsx
@@ -0,0 +1,9 @@
+import { Link } from "@mui/material";
+
+export default function Footer() {
+  return (
+    <Link color="inherit" underline="hover" href="#">
+      All Songs A-Z
+    </Link>
+  );
+}
