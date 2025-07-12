81c6d7f6615ac5c3318bfdd96c75bb650e5b4757
AmitaiRosenbaum <amitai.rosenbaum@outlook.com>
2025-07-12 23:15:37 +1000

    refactor upload script as class

--- pdf-processing-engine/src/scripts/api/upload-songs.py
+++ pdf-processing-engine/src/scripts/api/upload-songs.py
@@ -7,38 +7,46 @@ ROOT = Path(__file__).resolve().parents[2]
 API_URL = 'http://localhost:8000'
 
 
-def get_song_data():
-  all_artists = set()
-  all_songs = set()
+class SongUploader():
+  def __init__(self) -> None:
+    self.songs: list[str]
+    self.artists: list[str]
+    self.song_artists: dict[str, list[str]]
   
-  with open(ROOT / 'data' / 'corrected_songs.csv', 'r') as file:
-    next(file)
-    for line in file:
-      artist_names, song = line.split(';')
-      artists = [x.strip() for x in re.split(r'\se\s|,\s', artist_names)]
-      all_songs |= {song.strip()}
-      all_artists |= set(artists)
+  def get_song_data(self):
+    all_artists = set()
+    all_songs = set()
+    
+    with open(ROOT / 'data' / 'corrected_songs.csv', 'r') as file:
+      next(file)
+      for line in file:
+        artist_names, song = line.split(';')
+        artists = [x.strip() for x in re.split(r'\se\s|,\s', artist_names)]
+        all_songs |= {song.strip()}
+        all_artists |= set(artists)
 
-  all_artists = all_artists - {'TODO DELETE'}
-  all_songs = all_songs - {'TODO DELETE'}
-  all_artists = list(all_artists)
-  all_songs = list(all_songs)
-  all_artists.sort()
-  all_songs.sort()
+      all_artists = all_artists - {'TODO DELETE'}
+      all_songs = all_songs - {'TODO DELETE'}
+      all_artists = list(all_artists)
+      all_songs = list(all_songs)
+      all_artists.sort()
+      all_songs.sort()
+      
+    self.artists = all_artists
+    self.songs = all_songs
   
-  return all_artists, all_songs
-
-def upload_artists(artists: list[str]):
-  for artist in artists:
-    try:
-      response = requests.post(API_URL + "/songs/artist/", json={"name": artist})
-      print(response)
-    except requests.exceptions.RequestException as e:
-      print(e)
+  def upload_artists(self):
+    for artist in self.artists:
+      try:
+        response = requests.post(API_URL + "/songs/artist/", json={"name": artist})
+        print(response)
+      except requests.exceptions.RequestException as e:
+        print(e)
 
 def main():
-  artists, songs = get_song_data()
-  print(songs)
+  uploader = SongUploader()
+  uploader.get_song_data()
+  uploader.upload_artists()
   # 1. Upload to S3 bucket
   # 2. Upload artists via Django API
   # 3. Upload songs via Django API
