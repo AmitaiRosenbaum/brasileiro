9fc7a72a1538e256ccc70db3964e0094f85e4528
AmitaiRosenbaum <amitai.rosenbaum@outlook.com>
2026-05-01 23:04:52 +1000

    toggeable run migrations

--- back-end/entrypoint.sh
+++ back-end/entrypoint.sh
@@ -6,8 +6,11 @@ if [ "$#" -eq 0 ]; then
   set -- python manage.py runserver 0.0.0.0:8000
 fi
 
-if [ "$1" = "gunicorn" ]; then
+if [ "${RUN_MIGRATIONS_ON_START:-0}" = "1" ]; then
   python manage.py migrate --noinput
+fi
+
+if [ "$1" = "gunicorn" ]; then
   python manage.py collectstatic --noinput
 fi
 
